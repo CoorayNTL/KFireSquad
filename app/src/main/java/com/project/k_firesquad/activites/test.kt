e20c67d76084ddd21ec644000aa1a698f7920a8d
CoorayNTL <tl685998@gmail.com>
2023-04-28 14:30:53 +0530

    update the folder structure

--- app/src/main/java/com/project/k_firesquad/activites/test.kt
+++ app/src/main/java/com/project/k_firesquad/activites/test.kt
@@ -1,4 +1,29 @@
 package com.project.k_firesquad.activites
 
 class test {
+    val a = 1
+    val b = 2
+    val c = 3
+    val d = 4
+
+    fun test() {
+        val a = 1
+        val b = 2
+        val c = 3
+        val d = 4
+    }
+
+    fun test2() {
+        val a = 1
+        val b = 2
+        val c = 3
+        val d = 4
+    }
+
+    fun test3() {
+        val a = 1
+        val b = 2
+        val c = 3
+        val d = 4
+    }
 }
